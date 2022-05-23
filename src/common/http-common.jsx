f44026715ea2b6676a4546f03aa06f7c4f28f3e0
timw <102154201+wan102@users.noreply.github.com>
2022-05-23 15:20:10 +0000

    fix bugs

--- src/common/http-common.jsx
+++ src/common/http-common.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:10833/api/v1/',
-  // baseURL: 'https://API-frontend.kenwan2112.repl.co/api/v1',
+  // baseURL: 'http://localhost:10833/api/v1/',
+  baseURL: 'https://API-frontend.kenwan2112.repl.co/api/v1',
   headers: {
     'Content-type': 'application/json',
     'Content-Length': '<calculated when request is sent>'

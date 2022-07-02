00576b24b51b531442dd26735dfb4e867acc7385
wan102 <102154201+wan102@users.noreply.github.com>
2022-07-02 20:30:15 +0800

    i delete all

--- src/components/home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Welcome! We are The Canine Shelter. We are providing something information about dogs.</h2>
-    </main>
-  );
-}
